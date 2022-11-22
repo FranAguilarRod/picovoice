37ce2e657d7f845fcbaf723c41172691a747c0fb
Francisco Aguilar <francisco.aguilar@scrm.eu>
2022-11-22 21:20:16 +0100

    Fix3

--- start_raspberry-pi.sh
+++ start_raspberry-pi.sh
@@ -2,7 +2,7 @@
 
 java -jar demo/java/build/libs/picovoice-mic-demo.jar \
 -a nPZc5tQYsrNLy8GO+yxso1E96MhjEdhPZXg+OEuB72Bi2Eff2sWSwA== \
--pm resources/porcupine/lib/common/porcupine_params_es.pv \
+-pm porcupine_params_es.pv \
 -k leopardo_raspberry-pi.ppn \
--rm resources/rhino/lib/common/rhino_params_es.pv \
+-rm rhino_params_es.pv \
 -c Custom_es_raspberry-pi_v2_1_0.rhn
