d93dc47981ff9dd68c03054162d12f6cf9ed9a19
Fran Aguilar <fraagurod@gmail.com>
2022-11-23 13:09:20 +0100

    Alfredo

--- start_raspberry-pi.sh
+++ start_raspberry-pi.sh
@@ -3,6 +3,6 @@
 java -jar demo/java/build/libs/picovoice-mic-demo.jar \
 -a nPZc5tQYsrNLy8GO+yxso1E96MhjEdhPZXg+OEuB72Bi2Eff2sWSwA== \
 -pm porcupine_params_es.pv \
--k leopardo_raspberry-pi.ppn \
+-k Alfredo_es_raspberry-pi_v2_1_0.ppn \
 -rm rhino_params_es.pv \
 -c Custom_es_raspberry-pi_v2_1_0.rhn
