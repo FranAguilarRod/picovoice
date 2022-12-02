0fd8615dab97c80815f6205b29089db76d490766
Fran Aguilar <fraagurod@gmail.com>
2022-12-02 14:32:45 +0100

    update accuracy

--- start_raspberry-pi.sh
+++ start_raspberry-pi.sh
@@ -5,4 +5,5 @@ java -jar demo/java/build/libs/picovoice-mic-demo.jar \
 -pm porcupine_params_es.pv \
 -k Alfredo_es_raspberry-pi_v2_1_0.ppn \
 -rm rhino_params_es.pv \
--c Custom_es_raspberry-pi_v2_1_0.rhn
+-c Custom_es_raspberry-pi_v2_1_0.rhn \
+-ps 0.3
