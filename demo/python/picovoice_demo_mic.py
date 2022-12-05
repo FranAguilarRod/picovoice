ca5bbcb3c3db78c3c82df140179ecc6a8fe93180
Fran Aguilar <fraagurod@gmail.com>
2022-12-05 14:29:56 +0100

    Python

--- demo/python/picovoice_demo_mic.py
+++ demo/python/picovoice_demo_mic.py
@@ -109,10 +109,13 @@ class PicovoiceDemo(Thread):
             # print('}\n')
             json_dumps = json.dumps(inference.slots)
             print(json_dumps)
-            Headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkZjQ1MjUzMGRmOTc0MmM5YTQzZTYxNDcwOGZiY2U0MiIsImlhdCI6MTY2OTEzOTc1NywiZXhwIjoxOTg0NDk5NzU3fQ.i05deYebPb6JfRQTVdAKFdnGFEtV1EL3ifK_Vqcm3YQ"}
+            headers = {
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkZjQ1MjUzMGRmOTc0MmM5YTQzZTYxNDcwOGZiY2U0MiIsImlhdCI6MTY2OTEzOTc1NywiZXhwIjoxOTg0NDk5NzU3fQ.i05deYebPb6JfRQTVdAKFdnGFEtV1EL3ifK_Vqcm3YQ",
+                "content-type": "application/json"
+            }
             requests.post(
                 "http://192.168.1.128:8123/api/events/rhasspy_%s" % inference.intent,
-                headers=Headers,
+                headers=headers,
                 json=json_dumps
             )
         else:
